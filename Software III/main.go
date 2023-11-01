6f1b220e5a0f17dfe03f0bbd11464e20fda1eef8
edison largo <edisonh.largog@uqvirtual.edu.co>
2023-11-01 13:41:44 -0500

    creacion de la bases de datos

--- Software III/main.go
+++ Software III/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"ToolHub/handlers"
+	"database/sql"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type User struct {
+	ID         int
+	Nombre     string
+	correo     string
+	Contraseña string
+}
+
+func dbConn() (db *sql.DB) {
+	db, err := sql.Open("sqlite3", "C:/Users/bklg1/OneDrive/Escritorio/Escritorio/Universidad/sotfware proyecto/AndresAristizabal_EdisonLargo_JuanCortes_MariaMinotta/Software III/base de datos/basededatos.db")
+
+	db.Exec("CREATE TABLE IF NOT EXISTS usuario (id INTEGER PRIMARY KEY AUTOINCREMENT,nombre VARCHAR(30) NOT NULL,correo VARCHAR(30) NOT NULL,contraseña VARCHAR(30) NOT NULL )")
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Println("Bases de datos conectada")
+
+	return db
+}
+
 func main() {
+
+	db := dbConn()
 
 	r := gin.Default()
 
@@ -24,4 +47,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 }
