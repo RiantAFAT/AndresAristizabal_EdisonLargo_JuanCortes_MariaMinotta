340d6c00bba351c55486f56da793d7a9dcd71281
edison largo <edisonh.largog@uqvirtual.edu.co>
2023-11-04 15:19:33 -0500

    pagina principal

--- Software III/handlers/principal_handler.go
+++ Software III/handlers/principal_handler.go
@@ -7,6 +7,40 @@ import (
 )
 
 func PrincipalPage(c *gin.Context) {
-	// Acceder a la sesión
-	c.HTML(http.StatusOK, "principal.html", nil)
+	// Obtén el término de búsqueda del formulario HTML
+	searchTerm := c.PostForm("search")
+
+	// Realiza una búsqueda en tus datos o base de datos para encontrar el producto por nombre
+	result := BuscarProductoPorNombre(searchTerm)
+
+	// Pasa el resultado a la plantilla HTML
+	c.HTML(http.StatusOK, "principal.html", gin.H{
+		"Result": result,
+	})
+}
+
+func BuscarProductoPorNombre(searchTerm string) *Producto {
+	// Implementa la lógica de búsqueda real aquí, busca el producto por nombre
+	// Devuelve el producto si se encuentra, o nil si no se encuentra
+	// Esto es un ejemplo simplificado
+	for _, producto := range Productos {
+		if producto.Nombre == searchTerm {
+			return &producto
+		}
+	}
+	return nil
+}
+
+type Producto struct {
+	Nombre       string
+	Descripcion  string
+	Calificacion float64
+	URLImagen    string
+	// Agrega otros campos relacionados con el producto según sea necesario
+}
+
+var Productos = []Producto{
+	Producto{"ChatGpt", "un modelo de lenguaje desarrollado por OpenAI.Estoy diseñado para comprender y generar texto en lenguaje natural en una variedad de temas y contextos", 4.5, "/static/imagenes/Chatgpt.png"},
+	Producto{"Producto 2", "Descripción del producto 2", 3.5, "product2.jpg"},
+	// Agrega más productos según tus necesidades
 }
--- Software III/main.go
+++ Software III/main.go
@@ -6,31 +6,30 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	ID         int
-	Nombre     string
-	correo     string
-	Contraseña string
-}
-
-func dbConn() (db *sql.DB) {
-	db, err := sql.Open("sqlite3", "C:/Users/bklg1/OneDrive/Escritorio/Escritorio/Universidad/sotfware proyecto/AndresAristizabal_EdisonLargo_JuanCortes_MariaMinotta/Software III/base de datos/basededatos.db")
-
-	db.Exec("CREATE TABLE IF NOT EXISTS usuario (id INTEGER PRIMARY KEY AUTOINCREMENT,nombre VARCHAR(30) NOT NULL,correo VARCHAR(30) NOT NULL,contraseña VARCHAR(30) NOT NULL )")
+func OpenConnection() (db *sql.DB) {
+	// Configuración de la conexión
+	dsn := "root:root@tcp(localhost:3306)/ToolHub"
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
+		// Manejar el error
+		log.Println("openerror")
 		panic(err.Error())
 	}
-	log.Println("Bases de datos conectada")
-
+	err = db.Ping()
+	if err != nil {
+		// Manejar el error
+		log.Println("error ping")
+	}
+	//db.Exec("INSERT INTO Usuario (nombre, correo, contraseña) VALUES (?, ?, ?)", "Hernan", "correo@example.com", "root")
 	return db
 }
 
 func main() {
 
-	db := dbConn()
+	db := OpenConnection()
 
 	r := gin.Default()
 
@@ -42,10 +41,15 @@ func main() {
 	r.GET("/register", handlers.RegisterPage)
 
 	r.POST("/login", handlers.Login)
+	r.POST("/registrarUsuario", func(c *gin.Context) {
+		handlers.RegistrarUsuario(c, db)
+	})
+	r.POST("/principal", handlers.PrincipalPage)
 
 	err := r.Run(":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer db.Close()
 }
